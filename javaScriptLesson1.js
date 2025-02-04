6f68f8c7a997d16c809ab98e4b42954a83585592
Alexandra Martinez <alexandra.m@cox.net>
2025-02-04 09:52:15 +0100

    Add .vscode/launch.json, javaScriptLesson1.js, and style.css

---
+++ javaScriptLesson1.js
@@ -0,0 +1,34 @@
+/*
+var a = 5;
+console.log(a);
+
+
+//let or const
+
+let b = 10;
+console.log(b);
+
+
+const c = "Hello World";
+console.log(c);
+
+var x = 5;
+var y = 10; 
+console.log('x=${x}, y=${y}');
+
+let x = 5;
+console.log(x=${x}, z=$,{z});
+let z = 10;
+
+//let w=3;
+console.log('w=${w}');
+let w=20;
+
+let a = 'Hello';
+ function myFunc(){
+    const b= 'World';;
+    return b;
+
+ }
+
+*/
