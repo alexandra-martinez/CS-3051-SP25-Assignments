71a320210bf22e0bf1d6b587a79c9b989f1904f1
Alexandra Martinez <alexandra.m@cox.net>
2025-05-09 15:31:04 +0200

    Fresh commit after fixing submodule issue

---
+++ CS-3051-SP25-Assignments/7-2-25.js
@@ -0,0 +1,17 @@
+/*function a(){
+    console.log('b');
+    function c(value) {
+        return '$' + value;
+    }
+    return c(1+2); // Assuming x and y are 1 and 2
+}
+
+function b(x, y) {
+    return x + y;
+}
+*/
+let array = [1,2,3,4,5];
+console.log(array.length);
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
